fbbd5c010e57d8db0788bf45e0651aab87f5c81d
Surbhi Kohli <surkohli@duosecurity.com>
2026-01-31 20:17:24 +0530

    added comment [skip ci]

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -4,7 +4,7 @@ import HelpArea from './HelpArea';
 
 function MainContent() {
   const [helpVisible, setHelpVisible] = useState(false);
-
+  // Functionn to toggle
   function toggleHelp() {
     setHelpVisible((isVisible) => !isVisible);
   }
